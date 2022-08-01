21ae505435bd795b9e0de39e4ed20cf2519dcc89
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-08-01 08:38:32 -0300

    Add files via upload

---
+++ Lucas e Polianna 01-08.sql
@@ -0,0 +1,273 @@
+
+-- set sql_set_updates = 0; -- Para poder excluir sem Where. 
+drop database dbDistribuidora;
+create database dbDistribuidora;
+use dbDistribuidora;
+
+create table tbCliente(
+	IdCli int primary key auto_increment,
+    NomeCli varchar(200) not null,
+    NumEnd numeric(6) not null,
+    CompEnd varchar(50),
+    CepCli numeric(8) not null
+);
+
+create table tbClientePF(
+	CPF numeric(11) primary key,
+    RG numeric(9) not null,
+    RG_Dig char(1) not null,
+    Nasc date not null,
+    IdCli int unique not null 
+); 
+
+create table tbClientePJ(
+	CNPJ numeric(14) primary key,
+    IE numeric(11) unique,
+    IdCli int unique not null 
+);
+
+create table tbProduto(
+    CodigoBarras numeric(14) primary key,
+    NomeProd varchar(200) not null,
+    Valor decimal(5,2) not null,
+    Qtd int
+);
+
+create table tbCompra(
+	CodigoCompra numeric(10) primary key,
+    DataCompra date default(current_timestamp()),
+    ValorTotal decimal(6,2) not null,
+    QtdTotal int not null,
+    NotaFiscal int,
+    IdCli int
+);
+
+create table tbNotaFiscal(
+	NotaFiscal int primary key,
+    TotalNota decimal(5,2) not null,
+    DataEmissao date not null
+);
+
+create table tbItemCompra(
+	NotaFiscal int,
+    CodigoBarras numeric(14),
+    ValorItem decimal(5,2) not null,
+    Qtd int not null,
+    primary key(NotaFiscal,CodigoBarras)
+);
+
+create table tbFornecedor(
+	IdFornecedor int auto_increment primary key,
+    CNPJ numeric(13) not null unique,
+    NomeFornecedor varchar(100) not null,
+    telefone numeric(11)
+);
+
+create table tbPedido(
+	NotaFiscalPedido int primary key,
+    DataCompra date not null,
+    ValorTotal decimal (6,2) not null,
+    QtdTotal int not null,
+    IdFornecedor int
+);
+
+create table tbPedidoProduto(
+	NotaFiscalPedido int,
+    CodigoBarras numeric(14),
+    primary key(NotaFiscalPedido,CodigoBarras)
+);
+
+create table tbEndereco(
+	CEP numeric(8) primary key,
+    Logradouro varchar(200),
+    IdBairro int not null,
+    IdCidade int not null,
+    IdUF int not null
+);
+
+create table tbBairro(
+    IdBairro int primary key auto_increment,
+    Bairro varchar(200) not null
+);
+
+create table tbCidade(
+    IdCidade int primary key auto_increment,
+    Cidade varchar(200) not null
+);
+
+create table tbUF(
+    IdUF int primary key auto_increment,
+    UF varchar(200) not null
+);
+
+alter table tbCliente add foreign key (CepCli) references tbEndereco(CEP);
+
+alter table tbClientePF add foreign key (IdCli) references tbCliente(IdCli);
+
+alter table tbClientePJ add foreign key (IdCli) references tbCliente(IdCli);
+
+alter table tbCompra add foreign key (NotaFiscal) references tbNotaFiscal(NotaFiscal);
+alter table tbCompra add foreign key (IdCli) references tbCliente(IdCli);
+
+alter table tbItemCompra add foreign key (NotaFiscal) references tbCompra(NotaFiscal);
+alter table tbItemCompra add foreign key (CodigoBarras) references tbProduto(CodigoBarras);
+
+alter table tbPedido add foreign key (IdFornecedor) references tbFornecedor(IdFornecedor);
+
+alter table tbPedidoProduto add foreign key (NotaFiscalPedido) references tbPedido(NotaFiscalPedido);
+alter table tbPedidoProduto add foreign key (CodigoBarras) references tbProduto(CodigoBarras);
+
+alter table tbEndereco add foreign key (IdBairro) references tbBairro(IdBairro);
+alter table tbEndereco add foreign key (IdCidade) references tbCidade(IdCidade);
+alter table tbEndereco add foreign key (IdUF) references tbUF(IdUF);
+
+ -- drop procedure spInsertCidade; -- Salvação
+
+delimiter $$
+create procedure spInsertFornecedor (vCNPJ numeric(13), vNomeFornecedor varchar(100) , vTelefone numeric(11))
+begin
+	insert into tbFornecedor(CNPJ, NomeFornecedor, telefone) values(vCNPJ, vNomeFornecedor, vTelefone);
+end
+$$
+
+describe tbCidade;
+delimiter $$
+create procedure spInsertCidade(vCidade varchar(200))
+begin
+	insert into tbCidade(Cidade) values (vCidade);
+end
+$$
+
+describe tbUF;
+delimiter $$
+create procedure spInsertUF(vIdUf int, vEstado varchar(200))
+begin
+	insert into tbUF(UF) values (vEstado);
+end
+$$
+
+describe tbBairro;
+delimiter $$
+create procedure spInsertBairro(vIdBairro int, vBairro varchar(200))
+begin
+	insert into tbBairro(Bairro) values (vBairro);
+end
+$$
+
+describe tbProduto;
+delimiter $$
+create procedure spInsertProduto(vCodigoBarras decimal(14,0), vNome varchar(200), vValor decimal(5,2), vQtd int)
+begin
+	insert into tbProduto(CodigoBarras,NomeProd,Valor,Qtd) values (vCodigoBarras,vNome,vValor,vQtd);
+end
+$$
+
+
+drop procedure spInsertEndereco;
+describe tbEndereco;
+delimiter $$
+create procedure spInsertEndereco(vCep decimal(8,0),vLogradouro varchar(200),vBairro varchar(200), vCidade varchar(200), vEstado varchar(200))
+begin
+
+	declare dBairro int;
+	declare dCidade int;
+	declare dEstado int;
+	if not exists(select IdUf from tbUf where UF = vEstado) then
+		call spInsertUf(vEstado);
+	end if;
+    if not exists(select IdCidade from tbCidade where Cidade = vCidade) then
+		call spInsertUf(vCidade);
+	end if;
+    if not exists(select IdBairro from tbUf where UF = vEstado) then
+		call spInsertUf(vEstado);
+	end if;
+
+	if not exists(select * from tbEndereco where Cep = vCep) then
+		set dBairro = (select idBairro from tbBairro where bairro = vBairro);
+		set dCidade = (select idCidade from tbCidade where Cidade = vCidade);
+		set dEstado = (select idUf from tbuf where uf = vEstado);
+		insert into tbEndereco(CEP,Logradouro,IdBairro,IdCidade,IdUF) values (vCep,vLogradouro, dBairro ,dCidade,dEstado);
+	end if;
+	
+end
+$$
+
+select *from tbendereco;
+
+call spInsertFornecedor(1245678937123, "Revenda Chico Loco", 11934567897);
+call spInsertFornecedor(1345678937123, "José Faz Tudo S/A", 11934567898);
+call spInsertFornecedor(1445678937123, "Vadalto Entregas", 11934567899);
+call spInsertFornecedor(1545678937123, "Astrogildo das Estrelas", 11934567800);
+call spInsertFornecedor(1645678937123, "Amoroso e Doce", 11934567801);
+call spInsertFornecedor(1745678937123, "Marcelo Dedal", 11934567802);
+call spInsertFornecedor(1845678937123, "Franciscano Cachaça", 11934567803);
+call spInsertFornecedor(1945678937123, "Joãozinho Chupeta", 11934567804);
+
+select * from tbFornecedor;
+
+call spInsertCidade("Rio de Janeiro");
+call spInsertCidade("São Carlos");
+call spInsertCidade("Campinas");
+call spInsertCidade("Franco da Rocha");
+call spInsertCidade("Osasco");
+call spInsertCidade("Pirituba");
+call spInsertCidade("Lapa");
+call spInsertCidade("Ponta Grossa");
+call spInsertCidade("São Paulo");
+call spInsertCidade("Barra Mansa");
+
+
+
+select * from tbCidade;
+
+call spInsertUF("SP");
+call spInsertUF("RJ");
+call spInsertUF("RS");
+
+select * from tbUF;
+
+call spInsertBairro("Aclimação");
+call spInsertBairro("Capão Redondo");
+call spInsertBairro("Pirituba");
+call spInsertBairro("Liberdade");
+call spInsertBairro("Lapa");
+call spInsertBairro("Penha");
+call spInsertBairro("Consolação");
+call spInsertBairro("Barra Funda");
+
+select * from tbBairro;
+
+call spInsertProduto(12345678910111,'Rei de Papel Mache',54.61,120);
+call spInsertProduto(12345678910112,'Bolinha de Sabão',100.45,120);
+call spInsertProduto(12345678910113,'Barro Bate Bate',44.00,120);
+call spInsertProduto(12345678910114,'Bola Furada',10.00,120);
+call spInsertProduto(12345678910115,'Maçã Laranja',99.44,120);
+call spInsertProduto(12345678910116,'Boneco do Hitler',124.00,200);
+call spInsertProduto(12345678910117,'Farinha de Surui',50.00,200);
+call spInsertProduto(12345678910118,'Zelador de Cemitério',24.50,100);
+
+select * from tbProduto;
+/*
+call spInsertProduto('cep,Lougradouro,Bairro,cidade,uf');
+call spInsertProduto(12345054,'Lougra',,120);
+call spInsertProduto(12345054,'Lougra',,120);
+call spInsertProduto(12345054,'Lougra',,120);
+call spInsertProduto(12345054,'Lougra',,120);
+call spInsertProduto(12345054,'Lougra',,120);
+call spInsertProduto(12345054,'Lougra',,120);
+call spInsertProduto(12345054,'Lougra',,120);
+call spInsertProduto(12345054,'Lougra',,120);
+select * from tbEndereco;*/
+
+call spInsertEndereco(12345050, "Rua da Federal", "Lapa", "São Paulo", "SP");
+call spInsertEndereco(12345051, "Av Brasil", "Lapa", "Campinas", "SP");
+call spInsertEndereco(12345052, "Rua Liberdade", "Consolação", "São Paulo", "SP");
+call spInsertEndereco(12345053, "Av Paulista", "Penha", "Rio de Janeiro", "RJ");
+call spInsertEndereco(12345054, "Rua Ximbú", "Penha", "Rio de Janeiro", "RJ");
+call spInsertEndereco(12345055, "Rua Piu X1", "Penha", "Campinas", "SP");
+call spInsertEndereco(12345056, "Rua chocolate", "Aclimação", "Barra Mansa", "RJ");
+call spInsertEndereco(12345057, "Rua Pão na Chapa", "Barra Funda", "Ponta Grossa", "RS");
+
+
+
+
